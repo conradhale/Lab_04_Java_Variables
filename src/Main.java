980b3a68aded25760940a104de6f84b88f4d847f
conradhale <halec5@mail.uc.edu>
2022-09-15 19:10:59 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+  public static void main(String[] args) {
+    int intOperandA = 3;
+    int intOperandB = 2;
+
+    int intSum = intOperandA + intOperandB;
+    System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+    int intProduct = intOperandA * intOperandB;
+    System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+    int intDifference = intOperandA - intOperandB;
+    System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+    int intQuotient = intOperandA / intOperandB;
+    System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+    int intModulo = intOperandA % intOperandB;
+    System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+    double doubleOperandA = 2.50;
+    double doubleOperandB = 1.25;
+
+    double doubleSum = doubleOperandA + doubleOperandB;
+    System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+    double doubleProduct = doubleOperandA * intOperandB;
+    System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+    double doubleDifference = doubleOperandA - doubleOperandB;
+    System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+    double doubleQuotient = doubleOperandA / doubleOperandB;
+    System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+    double lunchCost = 10.25;
+    System.out.println("The cost of my lunch is " + lunchCost);
+
+    int kidsInFamily = 4;
+    System.out.println("There are " + kidsInFamily + " kids in my family");
+
+    double gallonGasPrice = 3.25;
+    System.out.println("The price of a gallon of gas is: " + gallonGasPrice);
+
+    int favNum = 9;
+    System.out.println("My favorite number is " + favNum);
+
+    int shoeSize = 10;
+    System.out.println("My shoe size is " + shoeSize);
+
+    String birthMonth = "July";
+    System.out.println("I was born in " + birthMonth);
+
+    String fullName = "Conrad Hale";
+    System.out.println("My full name is " + fullName);
+  }
+}
